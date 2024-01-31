4ce00dda28fb303dad7048ab2d42c52c41d35291
alwialdi9 <alwi.aldisyach@gmail.com>
2024-01-31 22:36:08 +0700

    add home and game controller

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'game/index'
+    get '/', to: "home#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
